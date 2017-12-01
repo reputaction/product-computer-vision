08eedd26d6f5993f4e6465400ede8d0094022d9b
kevin0228ca <kevin0228ca@gmail.com>
2017-12-01 11:34:44 +0900

    main activity hold mai fragment

--- app/src/main/java/com/reputaction/product_computer_vision/MainActivity.kt
+++ app/src/main/java/com/reputaction/product_computer_vision/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentManager.beginTransaction().replace(R.id.container, MainFragment.newInstance()).commit()
     }
 }
